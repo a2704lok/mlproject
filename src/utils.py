84dad5115f546dae5a733c12ab28b23df716ccf7
a2704lok <alokkumarsingh230@gmail.com>
2025-05-18 22:49:08 +0530

    hyperparameter added

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ from src.exception import CustomException
 from src.logger import logging
 import pickle
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV
+from dataclasses import dataclass
 
 
 def save_object(file_path, obj):
@@ -21,20 +23,34 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
-    """
-    Evaluate the models and return the model report.
-    """
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
-        for model_name, model in models.items():
-            model.fit(X_train, y_train)
-            y_pred = model.predict(X_test)
-            mse = mean_squared_error(y_test, y_pred)
-            r2 = r2_score(y_test, y_pred)
-            report[model_name] = r2
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
         return report
-    
+
     except Exception as e:
         raise CustomException(e, sys)
     
